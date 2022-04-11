c4906be133b40cf7fdc46c0f1a7921441bacebad
unknown <nsrinivas06@gmail.com>
2022-04-11 16:52:34 +0530

    Structure Changed/ Javascript added

--- app.js
+++ app.js
@@ -3,23 +3,28 @@ const path = require('path');
 var app = express();
 const port = 3000
 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static(__dirname + "/public"))
 
 const tf = require('@tensorflow/tfjs');
 const tfnode = require('@tensorflow/tfjs-node');
 
 async function loadModel(){
-    const handler = tfnode.io.fileSystem('playground/model.json');
+    const handler = tfnode.io.fileSystem('model_saves/tensorflowjs_model/model.json');
     const model = await tf.loadLayersModel(handler);
-    console.log(model);
-    console.log("Model loaded")
+    // console.log(model);
+    console.log("Model loaded");
+    return model;
 }
 
 
 app.use('/', function (req, res) {
-    loadModel();
-    res.sendFile(__dirname + '/sample.html');
+    // var model = loadModel();
+    res.render(__dirname + '/public/sample.html', {sample: "Text"});
 });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
