28c23e65dd7133fd2306968ab192439926581241
unknown <nsrinivas06@gmail.com>
2022-04-12 17:44:34 +0530

    Documentation for Web app added

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ app.use(express.static(__dirname + "/public"))
 const tf = require('@tensorflow/tfjs');
 const tfnode = require('@tensorflow/tfjs-node');
 
-async function loadModel(){
-    const handler = tfnode.io.fileSystem('model_saves/tensorflowjs_model/model.json');
-    const model = await tf.loadLayersModel(handler);
-    // console.log(model);
-    console.log("Model loaded");
-    return model;
-}
+// async function loadModel(){
+//     const handler = tfnode.io.fileSystem('model_saves/tensorflowjs_model/model.json');
+//     const model = await tf.loadLayersModel(handler);
+//     // console.log(model);
+//     console.log("Model loaded");
+//     return model;
+// }
 
 
 app.use('/', function (req, res) {
